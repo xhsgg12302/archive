5a7e284be81cbe348e9d4fb460cf610e62ab050f
XHS-12302 <xhsgg12302@gmail.com>
2024-12-23 04:30:11 +0800

    perf: add url param `side=false` to control menu hidden or show

--- assets/dist/menu/menu-init.js
+++ assets/dist/menu/menu-init.js
@@ -1,16 +1,45 @@
 // override themes/amethyst/assets/menu-reset.js
 
-(function() {
+(function () {
     function expandParents(element) {
-      var parentLi = element.closest('ul').closest('li');
-      while (parentLi && parentLi.closest('nav')) {
-          parentLi.querySelector('input').checked = true;
-          parentLi = parentLi.closest('ul').closest('li');
-      }
+        var parentLi = element.closest('ul').closest('li');
+        while (parentLi && parentLi.closest('nav')) {
+            parentLi.querySelector('input').checked = true;
+            parentLi = parentLi.closest('ul').closest('li');
+        }
     }
-  
-    const target = window.location.pathname;
-    if(target === '/') return;
+
+    function parseQuery(query) {
+        var res = {};
+        query = query.trim().replace(/^(\?|#|&)/, '');
+        if (!query) { return res; }
+
+        query.split('&').forEach(function (param) {
+            var parts = param.replace(/\+/g, ' ').replace(/#.*/, '').split('=');
+            res[parts[0]] = parts[1] && decodeURIComponent(parts[1]);
+        });
+        return res;
+    }
+
+    function parse(path) {
+        if (path === void 0) path = location.href;
+        var query = '', queryIndex = path.indexOf('?');
+        if (queryIndex >= 0) {
+            query = path.slice(queryIndex + 1);
+            path = path.slice(0, queryIndex);
+        }
+        return { path: path, pathname: location.pathname, query: parseQuery(query) };
+    };
+
+    const route = parse();
+    if (route.query.side === 'false') {
+        let menuAside = document.querySelector('aside.book-menu');
+        menuAside.style.display = 'none';
+        return;
+    }
+
+    const target = route.pathname;
+    if (target === '/') return;
     const mainUL = document.querySelector("aside .book-menu-content nav>ul");
     const sel = 'a[href="' + target + '"]';
     const targetA = mainUL.querySelector(sel);
